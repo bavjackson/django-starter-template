2084cfc03751f367db06da68b475e7b406fafafa
bavjackson <33766488+bavjackson@users.noreply.github.com>
2023-09-30 19:34:05 +0000

    added python-dotenv, updated settings to use env vars, added custom user model

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # django-starter-template
 
-A starter template for a django project that includes
+A starter template for a django project that includes the following packages.
 
 - Pytest
 - Coverage
@@ -10,9 +10,11 @@ A starter template for a django project that includes
 - AlpineJS
 - Python-dotenv
 
+It also has the boilerplate for a custom user model in a `users` app.
+
 ## How To
 
 1. Clone the repo
 2. Run `pip install -r requirements.txt` to install python packages
 3. Run `playwright install` to install browsers for end to end tests
-4. Add a .env file with any necessary secrets
+4. Add a .env file and add values corresponding to those in .env.example
---
+++ users/tests/test_models.py
@@ -0,0 +1,42 @@
+import pytest
+from django.contrib.auth import get_user_model
+
+def test_create_user():
+    email = 'normal@user.com'
+    User = get_user_model()
+    user = User.objects.create_user(email=email, password='foo')
+    assert user.email == email
+    assert user.is_active
+    assert not user.is_staff
+    assert not user.is_superuser
+    try:
+        assert user.username is None
+    except AttributeError:
+        pass
+    with pytest.raises(TypeError):
+        # User needs to provide email and password
+        User.objects.create_user()
+    with pytest.raises(TypeError):
+        # Email cannot be empty
+        User.objects.create_user(email="")
+    with pytest.raises(TypeError):
+        # Email cannot be empty
+        User.objects.create_user(email="", password='foo')
+
+def test_create_superuser():
+    email = 'admin@user.com'
+    User = get_user_model()
+    admin = User.objects.create_superuser(email=email, password='foo')
+    assert admin.email == email
+    assert admin.is_active
+    assert admin.is_staff
+    assert admin.is_superuser
+    try:
+        assert admin.username is None
+    except AttributeError:
+        pass
+    with pytest.raises(ValueError):
+        User.objects.create_supeuser(
+            email=email, password='foo', is_superuser=False
+        )
+        
